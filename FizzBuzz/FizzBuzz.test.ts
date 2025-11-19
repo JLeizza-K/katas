905030d9e50b3f69b79b290bfe4226b6a1796c1d
Julen <jleizza@kopiustech.com>
2025-11-19 12:32:34 -0300

    final fixes in fizzbuzz

--- FizzBuzz/FizzBuzz.test.ts
+++ FizzBuzz/FizzBuzz.test.ts
@@ -6,6 +6,10 @@ describe("Dividers game", () => {
     { name: "fizz", divider: 3 },
     { name: "buzz", divider: 5 },
   ]);
+  const game2 = new Game([
+    { name: "pato", divider: 7 },
+    { name: "nato", divider: 8 },
+  ]);
 
   it("Should return correct number of fizzes", () => {
     const result = game.fizzBuzz(10);
@@ -15,5 +19,15 @@ describe("Dividers game", () => {
   it("Should return correct number of buzzes", () => {
     const result = game.fizzBuzz(10);
     expect(result[1]).to.equal("buzz count is 2");
+  });
+
+  it("Should return correct number of patos", () => {
+    const result = game2.fizzBuzz(21);
+    expect(result[0]).to.equal("pato count is 3");
+  });
+
+  it("Should return correct number of natos", () => {
+    const result = game2.fizzBuzz(64);
+    expect(result[1]).to.equal("nato count is 8");
   });
 });
